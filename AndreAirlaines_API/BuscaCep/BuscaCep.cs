a0a475028b2271d371806b403cf7b11fbbf0dc87
EvertonFabricio <everton.5by5@gmail.com>
2022-04-01 12:13:13 -0300

    Criado a classe busca cep

---
+++ AndreAirlaines_API/BuscaCep/BuscaCep.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AndreAirlaines_API.BuscaCep
+{
+    public class BuscaCep
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main()
+        {
+            Console.Write("CEP: ");
+            var cep = Console.ReadLine();
+            try
+            {
+
+                HttpResponseMessage response = await client.GetAsync("https://viacep.com.br/ws/" + cep + "/json/");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+               
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+
+        }
+    }
+
+}
+
